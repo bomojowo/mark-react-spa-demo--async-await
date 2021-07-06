61f4bf2deba31654e2c95aa9a02a1dbe6ce20dd7
Bukola Omojowo <bomojowo@gmail.com>
2021-07-06 15:07:36 +0100

    Added comments and function for exercise

--- src/1-async-return.ts
+++ src/1-async-return.ts
@@ -1,12 +1,18 @@
-function greet(): void {
-  console.log("hello world"!);
+// function greet(): void {
+//   console.log("hello world"!);
+// }
+
+// async function promiseToGreet(): Promise<void> {
+//   console.log("hello world"!);
+// }
+
+async function promiseToAdd(a: number, b:number): Promise<number>{
+  return a + b
 }
 
-async function promiseToGreet(): Promise<void> {
-  console.log("hello world"!);
-}
-
-greet();
+//greet();
+console.log(promiseToAdd(2,3))
 // promiseToGreet()
 // greet().then(() => console.log("CANNOT do this"));
 // promiseToGreet().then(() => console.log("CAN do this"));
+
